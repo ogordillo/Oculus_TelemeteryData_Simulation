7cf7f6de9e12f589fa6900dbd651dfef9a14b000
csuser <csuser>
2017-04-22 01:04:14 -0600

    Initial Scene with Car

---
+++ Assets/Models/GV/Scripts/ScriptLights.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+
+
+
+public class ScriptLights : MonoBehaviour {
+
+	public bool blink;
+	[Header("Lights")]
+	public bool frontLightsOn;
+	public bool backLightsOn;
+	public GameObject frontLighs;
+	public GameObject backLights;
+
+	private IEnumerator coroutine;
+
+
+	void Start () {
+		blink 			= false;
+		frontLightsOn 	= false;
+		backLightsOn 	= false;
+
+		coroutine = WaitAndPrint(1.0f);
+		StartCoroutine(coroutine);
+	
+	}
+	
+
+	void Update () {
+		if (blink) {
+			TurnOnFrontLights ();
+			TurnOnBackLights ();
+		}
+		
+	}
+
+	public void TurnOnFrontLights()
+	{
+		if (frontLightsOn) {
+			frontLighs.SetActive (true);
+		} else {
+			frontLighs.SetActive (false);
+		}
+	}
+
+	public void TurnOnBackLights()
+	{
+		if (backLightsOn) {
+			backLights.SetActive (true);
+		} else {
+			backLights.SetActive (false);
+		}
+	}
+
+
+
+	private IEnumerator WaitAndPrint(float waitTime) {
+		while (true) {
+			yield return new WaitForSeconds(waitTime);
+			frontLightsOn = !frontLightsOn;
+			backLightsOn = !backLightsOn;
+		}
+	}
+
+
+
+}
